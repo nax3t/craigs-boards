cbaf8d6a0ea933896008303064b1368033c5ab51
Ian Schoonover <iantskon@gmail.com>
2017-08-26 17:19:41 -0500

    Add new routes to app.js

--- app.js
+++ app.js
@@ -5,8 +5,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var index 		= require('./routes/index');
+var users 		= require('./routes/users');
+var posts 		= require('./routes/posts');
+var comments  = require('./routes/comments');
 
 var app = express();
 
@@ -24,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/posts', posts);
+app.use('/posts/:id/comments', comments);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

562ec9317a71a1216f0ad8411798a0893d2e0378
Ian Schoonover <iantskon@gmail.com>
2017-09-04 13:08:18 -0500

    Add async middleware to posts routes and switch to arrow functions

--- middleware/index.js
+++ middleware/index.js
@@ -1,6 +1,11 @@
 module.exports = {
+	asyncMiddleware: fn =>
+	  (req, res, next) => {
+	    Promise.resolve(fn(req, res, next))
+	      .catch(next);
+		},
 	// route middleware to make sure a user is logged in
-	isLoggedIn: function(req, res, next) {
+	isLoggedIn: (req, res, next) => {
 	    // if user is authenticated in the session, carry on 
 	    if(req.isAuthenticated()) return next();
 	    // set redirectTo on session so user can go back to original destination after login
@@ -9,7 +14,7 @@ module.exports = {
 	    req.flash('error', 'You need to be logged in to do that!');
 	    res.redirect('/login');
 	},
-	sanitizeBody: function(req, res, next) {
+	sanitizeBody: (req, res, next) => {
 		if(req.body.post.description) {
 			req.body.post.description = req.sanitize(req.body.post.description);
 		}
--- routes/index.js
+++ routes/index.js
@@ -3,43 +3,48 @@ const router = express.Router();
 const passport = require('passport');
 const User = require('../models/user');
 
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express', page: 'home' });
 });
 
-router.get('/signup', function(req, res) {
+router.get('/signup', (req, res) => {
   res.render('users/signup', {title: 'User Sign-up', page: 'signup', username: '', email: ''});
 });
 
-router.post('/signup', function(req, res) {
-	var newUser = new User({username: req.body.username, email: req.body.email});
-	User.register(newUser, req.body.password, function(err, user){
+router.post('/signup', (req, res) => {
+	let newUser = new User({username: req.body.username, email: req.body.email});
+	User.register(newUser, req.body.password, (err, user) =>{
 	  if(err){
-			// return out of callback and render the signupview with 
+			// return out of callback and render the signup view with 
 			// error message as flash and prefilled form inputs for username and email
-	    return res.render('users/signup', {error: err.message, username: req.body.username, email: req.body.email});
+	    return res.render('users/signup', 
+        {
+          error: err.message,
+          username: req.body.username,
+          email: req.body.email
+        }
+      );
 	  }
-	  passport.authenticate('local')(req, res, function(){
+	  passport.authenticate('local')(req, res, () =>{
 			res.redirect('/'); 
 	  });
 	});
 });
 
-router.get('/login', function(req, res) {
+router.get('/login', (req, res) => {
 	res.render('users/login', {title: 'User Login', page: 'login'});
 });
 
-//handling login logic
-router.post('/login', function(req, res, next) {
-  passport.authenticate('local', function(err, user, info) {
+router.post('/login', (req, res, next) => {
+  passport.authenticate('local', (err, user, info) => {
     if (err) { return next(err); }
     if (!user) { 
     	req.flash('error', 'Incorrect username or password')
     	return res.redirect('/login');
     }
-    req.logIn(user, function(err) {
+    req.logIn(user, (err) => {
       if (err) { return next(err); }
-      var redirectTo = req.session.redirectTo ? req.session.redirectTo : '/';
+      let redirectTo = req.session.redirectTo ? req.session.redirectTo : '/';
       delete req.session.redirectTo;
       req.flash('success', 'Successfully logged in!');
       res.redirect(redirectTo);
@@ -47,7 +52,7 @@ router.post('/login', function(req, res, next) {
   })(req, res, next);
 });
 
-router.get('/logout', function(req, res){
+router.get('/logout', (req, res) => {
  req.logout();
  req.flash('success', 'See you next time!');
  res.redirect('/');
